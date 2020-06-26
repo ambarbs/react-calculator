34e63131e74d8fef5cb2481752e2a2847ef46463
Ambar Bose <ambar.bs@gmail.com>
2020-06-26 23:04:15 +1000

    Updates

---
+++ precache-manifest.d8f81bca8f260365003acea3d9ad412b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7914bf1660d76007552a2b8311d960a6",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "6f2d46b340f2f0274a56",
+    "url": "/react-calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a5e8e92687ee49f330a0",
+    "url": "/react-calculator/static/js/2.7f79d5f9.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/react-calculator/static/js/2.7f79d5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2d46b340f2f0274a56",
+    "url": "/react-calculator/static/js/main.417bd41c.chunk.js"
+  },
+  {
+    "revision": "909d0137fa7114c15e08",
+    "url": "/react-calculator/static/js/runtime-main.a14a8bab.js"
+  }
+]);
