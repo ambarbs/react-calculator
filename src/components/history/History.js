4128b09905a3e2ade0bb6f555b8393c1723ff5de
Ambar Bose <ambar.bs@gmail.com>
2020-06-29 12:07:21 +1000

    show result with comma in history

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { HistoryItem, HistoryWrapper } from './History.styles';
+import { numberWithCommas } from '../calculator/utils';
 
 const History = ({ history }) => (
   <HistoryWrapper>
@@ -9,7 +10,7 @@ const History = ({ history }) => (
         <HistoryItem paddingRight="1rem">
           {`${h.displayText} =`}
         </HistoryItem>
-        <HistoryItem color="black">{h.result}</HistoryItem>
+        <HistoryItem color="black">{numberWithCommas(h.result)}</HistoryItem>
       </>
     ))}
   </HistoryWrapper>
